4b59450b5e81960c0c257c7ebd0eb3990f2a8ccc
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-08-18 10:53:54 +0200

    chore(charts-native): scaffold project

---
+++ packages/pluggableWidgets/charts-native/src/Charts.tsx
@@ -0,0 +1,6 @@
+import { createElement, ReactElement } from "react";
+import { ChartsProps } from "../typings/ChartsProps";
+
+export function Charts(props: ChartsProps<undefined>): ReactElement | null {
+    return null;
+}
---
+++ packages/pluggableWidgets/charts-native/typings/ChartsProps.d.ts
@@ -0,0 +1,14 @@
+/**
+ * This file was generated from Charts.xml
+ * WARNING: All changes made to this file will be overwritten
+ * @author Mendix UI Content Team
+ */
+export interface ChartsProps<Style> {
+    name: string;
+    style: Style[];
+}
+
+export interface ChartsPreviewProps {
+    class: string;
+    style: string;
+}
