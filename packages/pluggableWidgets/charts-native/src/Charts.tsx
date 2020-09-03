6adb00777c68adb28d5ad60c709851598e4084ec
Isa Isaku <16718722+IIsaku@users.noreply.github.com>
2020-09-03 09:48:19 +0200

    chore(charts-native): add dss property linking

--- packages/pluggableWidgets/charts-native/src/Charts.tsx
+++ packages/pluggableWidgets/charts-native/src/Charts.tsx
@@ -1,7 +1,32 @@
-import { createElement, ReactElement } from "react";
+import { createElement, ReactElement, useState, useEffect } from "react";
+import { ValueStatus } from "mendix";
 
 import { Charts as ChartsComponent } from "./components/Charts";
+import { ChartsProps } from "../typings/ChartsProps";
 
-export function Charts(): ReactElement {
-    return <ChartsComponent />;
+export function Charts(props: ChartsProps<undefined>): ReactElement {
+    const { data, xAttribute, yAttribute } = props;
+
+    const [chartData, setChartData] = useState<Array<{ x: any; y: any }>>([]);
+
+    useEffect(() => {
+        // console.warn("effect", data.status);
+        // console.warn(data.totalCount);
+        if (data.status === ValueStatus.Available) {
+            setChartData(
+                data.items!.map(item => {
+                    const xValue = xAttribute(item);
+                    const yValue = yAttribute(item);
+                    if (xValue.status === ValueStatus.Available && yValue.status === ValueStatus.Available) {
+                        return { x: xValue.value, y: yValue.value };
+                    }
+                    return { x: 0, y: 0 };
+                })
+            );
+        }
+    }, [data, xAttribute, yAttribute]);
+
+    // console.warn(chartData);
+
+    return <ChartsComponent data={chartData} />;
 }
--- packages/pluggableWidgets/charts-native/src/components/Charts.tsx
+++ packages/pluggableWidgets/charts-native/src/components/Charts.tsx
@@ -3,21 +3,21 @@ import { StyleSheet, View } from "react-native";
 import {
     VictoryBar,
     VictoryChart,
-    VictoryAxis,
-    VictoryLabel,
-    VictoryGroup,
-    VictoryLegend,
-    Border
+    VictoryAxis
+    // VictoryLabel,
+    // VictoryGroup,
+    // VictoryLegend,
+    // Border
 } from "victory-native";
 
 // import { ChartsProps } from "../typings/ChartsProps";
 
-const data = [
-    { quarter: 1, earnings: 13000 },
-    { quarter: 2, earnings: 16500 },
-    { quarter: 3, earnings: 14250 },
-    { quarter: 4, earnings: 19000 }
-];
+// const data = [
+//     { quarter: 1, earnings: 13000 },
+//     { quarter: 2, earnings: 16500 },
+//     { quarter: 3, earnings: 14250 },
+//     { quarter: 4, earnings: 19000 }
+// ];
 
 const styles = StyleSheet.create({
     container: {
@@ -27,10 +27,16 @@ const styles = StyleSheet.create({
     }
 });
 
-export function Charts(): ReactElement {
+interface ChartsProps {
+    data: Array<{ x: any; y: any }>;
+}
+
+export function Charts(props: ChartsProps): ReactElement {
+    console.warn(props.data.length);
+
     return (
         <View style={styles.container}>
-            <VictoryChart width={400} padding={{ left: 90, top: 60, bottom: 60, right: 10 }} domainPadding={{ x: 25 }}>
+            {/* <VictoryChart width={400} padding={{ left: 90, top: 60, bottom: 60, right: 10 }} domainPadding={{ x: 25 }}>
                 <VictoryLabel x={200} y={30} text="Bar chart" textAnchor={"middle"} style={{ fontSize: 20 }} />
 
                 <VictoryAxis
@@ -230,27 +236,29 @@ export function Charts(): ReactElement {
                         }}
                     />
                 </VictoryGroup>
-            </VictoryChart>
+            </VictoryChart> */}
 
-            <VictoryChart width={250} domainPadding={20}>
-                <VictoryAxis tickValues={[1, 2, 3, 4]} tickFormat={["Q1", "Q2", "Q3", "Q4"]} />
-                <VictoryAxis dependentAxis style={{ grid: { stroke: "#818e99", strokeWidth: 1 } }} />
+            {props.data.length === 0 ? null : (
+                <VictoryChart domainPadding={20}>
+                    {/* <VictoryAxis tickValues={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]} /> */}
+                    <VictoryAxis />
+                    <VictoryAxis dependentAxis style={{ grid: { stroke: "#818e99", strokeWidth: 1 } }} />
 
-                <VictoryGroup offset={15}>
+                    {/* <VictoryGroup offset={15}> */}
                     <VictoryBar
-                        data={data}
-                        x="quarter"
-                        y="earnings"
+                        data={props.data}
+                        x="x"
+                        y="y"
                         barWidth={10}
                         style={{
                             data: { fill: "#0595DB" }
                         }}
-                        animate={{
-                            duration: 2000
-                        }}
+                        // animate={{
+                        //     duration: 2000
+                        // }}
                     />
 
-                    <VictoryBar
+                    {/* <VictoryBar
                         data={data.map(datapoint => {
                             return {
                                 quarter: datapoint.quarter,
@@ -266,9 +274,10 @@ export function Charts(): ReactElement {
                         animate={{
                             duration: 2000
                         }}
-                    />
-                </VictoryGroup>
-            </VictoryChart>
+                    /> */}
+                    {/* </VictoryGroup> */}
+                </VictoryChart>
+            )}
         </View>
     );
 }
